4c015d62c7526f8208168bd6db6cf189de0890ed
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-07-28 19:21:18 -0300

    exercicio 3

---
+++ fundamentos/intruducao-a-javascript-e-logica-de-programacao/dia1-primeiros-passos/3-maior-de3-numeros.js
@@ -0,0 +1,16 @@
+let num1 = 2;
+let num2 = 4;
+let num3 = 6;
+
+if (num1 > num2 && num1 > num3){
+	console.log(num1 +" é maior que " + num2 + " e " + num3);
+}
+else if (num2 > num1 && num2 > num3 ){
+	console.log(num2 +" é maior que " + num1 + " e " + num3);
+}
+else if (num3 > num1 && num3 > num2){
+	console.log(num3 +" é maior que " + num1 + " e " + num2);
+}
+else {
+	consele.log("É igual");
+}
