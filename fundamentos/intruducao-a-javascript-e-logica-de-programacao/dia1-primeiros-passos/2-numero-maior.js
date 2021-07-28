e5a58cc2c3212995a61ce1f75a45e89204c46418
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-07-28 19:11:00 -0300

    exercicios 1 e 2

---
+++ fundamentos/intruducao-a-javascript-e-logica-de-programacao/dia1-primeiros-passos/2-numero-maior.js
@@ -0,0 +1,12 @@
+let num1 = 2;
+let num2 = 6;
+
+if (num1 > num2){
+	console.log(num1 +" é maior que " + num2 );
+}
+else if (num1 < num2){
+	console.log(num2 +" é maior que " + num1);
+}
+else {
+	consele.log("É igual");
+}
