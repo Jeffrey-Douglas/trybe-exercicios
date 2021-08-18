b9b77e577deb89143caec22ea7df05b3bf27a2fb
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-08-18 14:43:27 -0300

    document.getElementById atividade

---
+++ fundamentos/introducao-a-javascript-e-logica-de-programacao/dia5.1-dom-e-seletores/script.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "V de Vingança";
+document.getElementById("paragraph").style.color = "red";
+document.getElementById("subtitle").innerText = "Melhor filme"; 
