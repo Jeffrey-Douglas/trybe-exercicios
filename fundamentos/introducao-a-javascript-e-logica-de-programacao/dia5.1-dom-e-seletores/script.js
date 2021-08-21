96dac8f6fe677b54fce8d720456a71bdfb371132
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-08-21 17:55:00 -0300

    getElements & querySelector

--- fundamentos/introducao-a-javascript-e-logica-de-programacao/dia5.1-dom-e-seletores/script.js
+++ fundamentos/introducao-a-javascript-e-logica-de-programacao/dia5.1-dom-e-seletores/script.js
@@ -1,3 +1,21 @@
-document.getElementById("page-title").innerText = "V de Vingança";
-document.getElementById("paragraph").style.color = "red";
-document.getElementById("subtitle").innerText = "Melhor filme"; 
+// document.getElementById("page-title").innerText = "V de Vingança";
+// document.getElementById("paragraph").style.color = "red";
+// document.getElementById("subtitle").innerText = "Melhor filme"; 
+
+// let filmao = document.getElementsByClassName("paragrafo");
+// for (let i = 0; i < filmao.length; i += 1) {
+//     filmao[i].innerText = "filme incrivel";
+// }
+
+// document.getElementsByTagName("h4")[0].style.color = "blue";
+
+document.querySelector("#page-title").innerText = "V de Vingança";
+document.querySelector("#paragraph").style.color = "red";
+document.querySelector("#subtitle").innerText = "Melhor filme"; 
+
+let filmao = document.querySelectorAll(".paragrafo");
+for (let i = 0; i < filmao.length; i += 1) {
+    filmao[i].innerText = "filme incrivel";
+}
+
+document.querySelectorAll("h4")[0].style.color = "blue";
